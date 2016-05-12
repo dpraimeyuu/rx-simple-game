7d2421aceaa80e2b5ee37def79186a4eab27742c
dpraimeyuu <dam.plaza@gmail.com>
2016-05-12 17:53:03 +0200

    updated package.json, added startup files, configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var ENV = process.env.NODE_ENV;
+
+module.exports = {
+  entry: ( ENV == 'production' ?
+           ['./main']
+           :
+           [
+            'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/dev-server',
+            './main'
+           ]
+  ),
+  output: {
+    filename: './dist/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: __dirname,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: ( ENV == 'production' ?
+             [
+              new webpack.optimize.UglifyJsPlugin({minimize: true}),
+             ]
+             :
+             [new webpack.HotModuleReplacementPlugin()]
+  ),
+  devServer: {
+    contentBase: './',
+    hot: true
+  }
+};
